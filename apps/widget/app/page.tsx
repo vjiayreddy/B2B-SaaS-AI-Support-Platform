1b2f829708e739b29a4e23f383acf01784b6a315
vijayreddy <vijay1989reddy@gmail.com>
2025-10-22 21:34:33 +0530

    initial setup

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -5,10 +5,10 @@ export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World/web</h1>
+        <h1 className="text-2xl font-bold">Hello World/widget</h1>
         <Button size="sm">Button</Button>
-        <p>{add(10,20)}</p>
-      </div>
+        {<p>{add(10,20)}</p>}
+      </div>x
     </div>
   )
 }
